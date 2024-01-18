5a537aaae625eb30446f397236cb88ccae360ae5
dprogrammer <kpidibadavid1@gmail.comm>
2024-01-18 11:53:23 +0000

    update folder architecture

---
+++ api/routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TacheController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login')->name('login');
+    Route::post('/deconnexion', 'deconnexion');
+});
+
+Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
+    Route::controller(TacheController::class)->group(function () {
+        Route::get('/tache', 'index')->name('tache.index');
+        Route::post('/tache', 'store')->name('tache.store');
+        Route::get('/tache/{tache}', 'show')->name('tache.show');
+        Route::put('/tache/{tache}', 'update')->name('tache.update');
+        Route::delete('/tache/{tache}', 'delete')->name('tache.delete');
+    });
+});
